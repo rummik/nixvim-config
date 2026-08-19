532366be4dc2ead46eafd1d04c654d32842ae6a6
Maxine Zick <25956545+MaxTheMooshroom@users.noreply.github.com>
2026-08-19 07:57:54 -0400

    disable annoying indentation 'fixer'

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -2,7 +2,7 @@
   config.plugins.treesitter = {
     enable = true;
     #folding = true;
-    settings.indent.enable = true;
+    # settings.indent.enable = true;
     settings.highlight = {
       enable = true;
       additional_vim_regex_highlighting = false;
